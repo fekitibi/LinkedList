d9c226ae039803a865857f57ad79a9993d062e65
tibi <tiborfek7@gmail.com>
2020-07-04 12:01:55 +0200

    Initial Commit

---
+++ LinkedList/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LinkedList
+//
+//  Created by Tibor Fekete on 04/07/2020.
+//  Copyright © 2020 Tibor Fekete. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
