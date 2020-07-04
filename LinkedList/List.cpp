f69e38999e166c73052edd87f604bd78a4deb0a9
tibi <tiborfek7@gmail.com>
2020-07-04 13:48:27 +0200

    first commit

---
+++ LinkedList/List.cpp
@@ -0,0 +1,9 @@
+//
+//  List.cpp
+//  LinkedList
+//
+//  Created by Tibor Fekete on 04/07/2020.
+//  Copyright © 2020 Tibor Fekete. All rights reserved.
+//
+
+#include "List.hpp"
---
+++ LinkedList/List.hpp
@@ -0,0 +1,14 @@
+//
+//  List.hpp
+//  LinkedList
+//
+//  Created by Tibor Fekete on 04/07/2020.
+//  Copyright © 2020 Tibor Fekete. All rights reserved.
+//
+
+#ifndef List_hpp
+#define List_hpp
+
+#include <stdio.h>
+
+#endif /* List_hpp */
